ddf19077d31ed64496ef0f776134e4a75618710b
HasanH47 <cruweldevil@gmail.com>
2023-05-11 12:28:29 +0700

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>This is the about page</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Leo a diam
+        sollicitudin tempor id eu nisl. Vitae justo eget magna fermentum iaculis
+        eu non diam. Viverra suspendisse potenti nullam ac tortor vitae purus.
+        At varius vel pharetra vel turpis. Eget lorem dolor sed viverra ipsum
+        nunc aliquet bibendum. Neque aliquam vestibulum morbi blandit cursus
+        risus at ultrices. Quis auctor elit sed vulputate mi sit amet. Mi tempus
+        imperdiet nulla malesuada pellentesque elit. Nullam non nisi est sit
+        amet facilisis magna etiam. Lorem sed risus ultricies tristique nulla
+        aliquet enim tortor at. Accumsan in nisl nisi scelerisque eu ultrices
+        vitae auctor eu. Odio tempor orci dapibus ultrices in iaculis nunc.
+        Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin.
+        Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Consequat ac
+        felis donec et odio pellentesque. Volutpat est velit egestas dui id
+        ornare. Diam sollicitudin tempor id eu.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Leo a diam
+        sollicitudin tempor id eu nisl. Vitae justo eget magna fermentum iaculis
+        eu non diam. Viverra suspendisse potenti nullam ac tortor vitae purus.
+        At varius vel pharetra vel turpis. Eget lorem dolor sed viverra ipsum
+        nunc aliquet bibendum. Neque aliquam vestibulum morbi blandit cursus
+        risus at ultrices. Quis auctor elit sed vulputate mi sit amet. Mi tempus
+        imperdiet nulla malesuada pellentesque elit. Nullam non nisi est sit
+        amet facilisis magna etiam. Lorem sed risus ultricies tristique nulla
+        aliquet enim tortor at. Accumsan in nisl nisi scelerisque eu ultrices
+        vitae auctor eu. Odio tempor orci dapibus ultrices in iaculis nunc.
+        Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin.
+        Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Consequat ac
+        felis donec et odio pellentesque. Volutpat est velit egestas dui id
+        ornare. Diam sollicitudin tempor id eu.
+      </p>
+      <p>
+        Condimentum id venenatis a condimentum vitae sapien. Egestas dui id
+        ornare arcu odio. Id eu nisl nunc mi ipsum. Mattis nunc sed blandit
+        libero volutpat sed cras. Egestas integer eget aliquet nibh praesent.
+        Porttitor eget dolor morbi non arcu. Commodo nulla facilisi nullam
+        vehicula ipsum a arcu cursus vitae. In aliquam sem fringilla ut morbi
+        tincidunt. Libero enim sed faucibus turpis in eu mi. Lectus mauris
+        ultrices eros in cursus turpis massa tincidunt. Eget duis at tellus at
+        urna. Eget nunc scelerisque viverra mauris in aliquam sem. Egestas purus
+        viverra accumsan in nisl nisi. Vulputate eu scelerisque felis imperdiet
+        proin fermentum. Euismod elementum nisi quis eleifend quam.
+      </p>
+    </div>
+  );
+}
