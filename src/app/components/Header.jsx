2303786cae3113cab429d338fa65218c310fb426
HasanH47 <cruweldevil@gmail.com>
2023-05-11 12:54:58 +0700

    create the navbar component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,7 +7,7 @@ import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-2-6xl sm:mx-auto items-center py-6">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
